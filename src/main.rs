980228d4c61486601dc8f1afbd1c96dd526421fc
pembem22 <24252798+pembem22@users.noreply.github.com>
2024-01-18 21:27:45 +0200

    Close UDP connection on timeout

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::{collections::HashMap, env, str, sync::Arc};
+use std::{
+    collections::HashMap,
+    env, str,
+    sync::Arc,
+    time::{Duration, SystemTime},
+    sync::Mutex as StdMutex,
+};
 
 use tokio::{
     io::{split, AsyncReadExt, AsyncWriteExt},
     net::UdpSocket,
     sync::Mutex,
+    task::JoinHandle,
 };
 use tokio_serial::SerialPortBuilderExt;
 
@@ -12,45 +19,73 @@ use macaddr::MacAddr6;
 #[cfg(unix)]
 const DEFAULT_TTY: &str = "/dev/ttyUSB0";
 #[cfg(windows)]
-const DEFAULT_TTY: &str = "COM8";
+const DEFAULT_TTY: &str = "COM5";
+
+struct Tracker {
+    socket: Arc<UdpSocket>,
+    last_message: SystemTime,
+    task: JoinHandle<()>,
+}
 
 #[tokio::main]
 async fn main() -> tokio_serial::Result<()> {
     let mut args = env::args();
     let tty_path = args.nth(1).unwrap_or_else(|| DEFAULT_TTY.into());
 
-    let port = tokio_serial::new(tty_path, 921600).open_native_async()?;
+    let port = tokio_serial::new(tty_path, 115200).open_native_async()?;
     let (mut port_in, port_out) = split(port);
     let port_out = Arc::new(Mutex::new(port_out));
 
-    let mut trackers = HashMap::new();
+    let trackers_mutex: Arc<Mutex<HashMap<MacAddr6, Arc<StdMutex<Tracker>>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+
+    let task_trackers_mutex = trackers_mutex.clone();
+    tokio::spawn(async move {
+        loop {
+            let mut trackers = task_trackers_mutex.lock().await;
+            trackers.retain(|mac, tracker_mutex| {
+                let tracker = tracker_mutex.lock().unwrap();
+                let timed_out = SystemTime::now()
+                    .duration_since(tracker.last_message)
+                    .unwrap()
+                    >= Duration::from_secs(1);
+
+                if timed_out {
+                    println!("{mac} timed out");
+                    tracker.task.abort();
+                }
+
+                !timed_out
+            });
+            drop(trackers);
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+    });
 
     loop {
         let mut mac_buffer = [0u8; 6];
         port_in.read_exact(&mut mac_buffer).await?;
         let mac = MacAddr6::from(mac_buffer);
         let length = port_in.read_u16().await?;
-        println!("Packet from {mac} of length {length}");
+        let timestamp = port_in.read_u32().await?;
+        println!("{:#08} Packet from {mac} of length {length}", timestamp);
 
         let mut buffer = vec![0u8; length as usize];
         port_in.read_exact(&mut buffer).await?;
-        // println!("{:X?}", buffer);
+        println!("{:X?}", buffer);
 
-        let socket = if !trackers.contains_key(&mac) {
+        let mut trackers = trackers_mutex.lock().await;
+
+        let tracker_mutex = if !trackers.contains_key(&mac) {
             let socket = Arc::new(UdpSocket::bind("0.0.0.0:0").await?);
-            socket.connect("127.0.0.1:6969").await.unwrap();
+            socket.connect("127.0.0.1:6969").await?;
 
-            println!("Created new socket");
-
-            trackers.insert(mac, socket.clone());
-
+            let task_socket = socket.clone();
             let port_out = port_out.clone();
-
-            let socket_thread = socket.clone();
-            tokio::spawn(async move {
+            let future = async move {
                 loop {
                     let mut buffer = [0u8; 1024];
-                    let length = socket_thread.recv(&mut buffer).await.unwrap();
+                    let length = task_socket.recv(&mut buffer).await.unwrap();
 
                     println!("Packet to   {mac} of length {length}");
 
@@ -66,13 +101,25 @@ async fn main() -> tokio_serial::Result<()> {
 
                     // println!("{:X?}", &buffer[0..length]);
                 }
-            });
+            };
 
-            socket
+            let tracker = Arc::new(StdMutex::new(Tracker {
+                socket,
+                last_message: SystemTime::now(),
+                task: tokio::spawn(future),
+            }));
+
+            trackers.insert(mac, tracker.clone());
+            println!("Created new socket");
+
+            tracker
         } else {
             trackers.get(&mac).unwrap().to_owned()
         };
 
-        socket.send(&buffer).await?;
+        let mut tracker = tracker_mutex.lock().unwrap();
+
+        tracker.last_message = SystemTime::now();
+        tracker.socket.send(&buffer).await?;
     }
 }
